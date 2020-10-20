563f3ec9cfc98e4a3ade26186bfe7276a02483cc
rosezhao <rosezhao@berkeley.edu>
2020-10-20 21:53:54 +0800

    added two plots

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+from vega_datasets import data
+import geopandas as gpd
+import json
 
-st.title("Let's analyze some climate change data :)")
+st.title("Let's analyze some CO2 emission data :)")
 
 @st.cache  # add caching so we load the data only once
 def load_data():
@@ -13,15 +16,20 @@ def load_data():
     country_df = pd.read_csv(country_url)
     country_df.set_index("Country Code")
     climate = climate_df.merge(country_df, on='Country Code', how='inner')
+
+    country_loc = "https://gist.githubusercontent.com/tadast/8827699/raw/3cd639fa34eec5067080a61c69e3ae25e3076abb/countries_codes_and_coordinates.csv"
+    country_loc_df = pd.read_csv(country_loc)
+    country_loc_df.replace('"', '', regex=True, inplace=True)
+    climate = climate.merge(country_loc_df,left_on="Country Name",right_on = "Country", how = "inner")
     return climate
 df = load_data()
 
+countries = alt.topo_feature(data.world_110m.url, 'countries')
 
-st.write("Let's look at raw data in the Pandas Data Frame.")
-
-if st.checkbox("show raw data"):
+if st.checkbox("Show Raw Data"):
     st.write(df)
 
+alt.data_transformers.disable_max_rows()
 # #selections
 # # picked = alt.selection_single(on="mouseover")
 # picked = alt.selection_single(fields=["Species","Island"])
@@ -63,3 +71,87 @@ if st.checkbox("show raw data"):
 # # 	alt.Y(),
 # # 	alt.Color()
 # # 	)
+
+st.header("Explore the worldwide total CO2 emission and emission per capita trend!")
+slider = alt.binding_range(min=1990, max=2011, step=1)
+select_year = alt.selection_single(name = "Year",fields=['Year'],
+                                   bind=slider, init={'Year': 1990})
+
+c = alt.Chart(df).mark_geoshape(
+    stroke='#aaa', strokeWidth=0.25
+).encode(
+    color=alt.Color('CO2 emissions (kt):Q',title = "total CO2 emissions"),
+    tooltip = ["Country Name", "CO2 emissions (kt)", "CO2 emissions (metric tons per capita)"]
+).transform_lookup(
+    lookup='Country Name',
+    from_=alt.LookupData("https://raw.githubusercontent.com/KoGor/Map-Icons-Generator/master/data/world-110m-country-names.tsv", 'name', ['id',"name"])
+).transform_lookup(
+    lookup='id',
+    from_=alt.LookupData(countries, 'id', fields=["id","type", "properties", "geometry"])
+).project(
+    type="equirectangular"
+).properties(
+    width=900,
+    height=500,
+    title='Country Name'
+)
+
+percapita = alt.Chart(df).mark_circle().encode(
+    size=alt.Size('CO2 emissions (metric tons per capita):Q', title='CO2 emissions per capita'),
+    color = alt.value("red"),
+    longitude='Longitude (average):Q',
+    latitude='Latitude (average):Q'
+).transform_lookup(
+    lookup='Country Name',
+    from_=alt.LookupData("https://raw.githubusercontent.com/KoGor/Map-Icons-Generator/master/data/world-110m-country-names.tsv", 'name', ['id',"name"])
+).transform_lookup(
+    lookup='id',
+    from_=alt.LookupData(countries, 'id', fields=["id","type", "properties", "geometry"])
+).project(
+    type="equirectangular"
+).properties(
+    width=900,
+    height=500,
+    title='Country Name'
+)
+
+# input_dropdown = alt.binding_select(options = ["Europe & Central Asia","Sub-Saharan Africa"],name = "Region of ")
+# picked_bind = alt.selection_single(encodings = ["color"], bind = input_dropdown)
+# picked = alt.selection_single(empty='all', fields=['Country Name'])
+# brush = alt.selection_interval(encodings=["x"])
+
+all = alt.layer(c,percapita)\
+    .add_selection(select_year)\
+    .transform_filter(select_year )
+st.write(all)
+
+
+st.header("Try to compare the countries you're interested!")
+dataset = st.multiselect("Choose countries you want to explore!", ["China","United States","United Kingdom","India","Russian Federation","Australia","Qatar"], ["China","United States"])
+
+df = df[df["Country Name"].isin(dataset)]
+df = df[df['Year'] <2011]
+df = df[df['Year'] >1990]
+df = df[df['CO2 emissions (kt)'] >0]
+
+line = alt.Chart(df).mark_trail().encode(
+    x=alt.X('Year'),
+    y=alt.Y('CO2 emissions (kt)'),
+    color = alt.Color('Country Name:N',scale=alt.Scale(domain=dataset,type='ordinal'))
+    ,size = alt.Size('CO2 emissions (metric tons per capita):Q',title="CO2 emission per capita")
+    ,tooltip=["Country Name","CO2 emissions (kt)","CO2 emissions (metric tons per capita)","Year"]
+).properties(
+    width=900,
+    height=500,
+    title='Country Name'
+)
+
+labels = alt.Chart(df).mark_text(align='left', dx=3).encode(
+    alt.X('Year', aggregate='max'),
+    alt.Y('CO2 emissions (kt)', aggregate={'argmax': 'Year'}),
+    alt.Text('Country Name'),
+    alt.Color('Country Name:N', legend=None, scale=alt.Scale(domain=dataset,type='ordinal')),
+).properties(title='CO2 total emission and emission per capita', width=600)
+
+
+st.write(line+labels)
